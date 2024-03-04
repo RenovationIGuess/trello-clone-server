c3036f874214e5c1aefb4dca61f61fd414b47d07
Ramy <nguyentunglam724@gmail.com>
2024-03-04 23:48:39 +0700

    feat: init models

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,12 +1,14 @@
 /* eslint-disable no-unused-vars */
 import { StatusCodes } from 'http-status-codes'
+import { boardService } from '~/services/boardService'
 
 const createNew = () => {
   return async (req, res, next) => {
     try {
       // Redirect to service
+      const createdBoard = await boardService.createNew(req.body)
 
-      res.status(StatusCodes.CREATED).json({ message: 'Create new board' })
+      res.status(StatusCodes.CREATED).json(createdBoard)
     } catch (error) {
       next(error)
     }
